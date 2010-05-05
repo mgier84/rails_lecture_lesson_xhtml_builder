bac1990a2586b26c148895003f2a299862b57fa2
mgier84 <marcogier84@gmx.de>
2010-05-05 16:59:42 +0200

    spec erfuellt

--- lib/xhtml_builder.rb
+++ lib/xhtml_builder.rb
@@ -1,3 +1,27 @@
 class XHTMLBuilder
   
+  attr_accessor :content
+  
+  def initialize()
+    @content = ""
+  end
+
+  def check_block(*args)
+    if block_given? then
+      yield
+    else
+      unless(args[0].nil?)
+        @content << args[0] << "\n"
+      end
+    end
+  end
+
+  def method_missing(sym, *args, &block)
+    @content << "<#{sym}>\n"
+
+    check_block(*args, &block)
+    
+    @content << "</#{sym}>\n"
+  end
+  
 end
